d55eba482f342fc128ad2cde9b775211a36ec134
David Arnold <dja+djaboxx@sonic.net>
2018-09-27 11:28:58 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+//--------------------------------------------------------------------
+// Variables
+variable "username" {}
+variable "password" {}
+variable "service_name" {}
+variable "service_version" {}
+variable "docker_cmd" {}
+
+variable "registry" {
+	default = "https://index.docker.io/v1/"
+}
+variable "repo" {
+  default = "happypathway"
+}
+
+//--------------------------------------------------------------------
+// Modules
+module "chef_docker" {
+  source  = "app.terraform.io/Darnold-WalMart_Demo/chef-docker/packer"
+  version = "1.2.7"
+
+  chef_env = "_default"
+	docker_cmd = "${var.docker_cmd}"
+  docker_password = "${var.password}"
+  docker_registry = "${var.registry}"
+  docker_repo = "${var.repo}"
+  docker_username = "${var.username}"
+  service_name = "${var.service_name}"
+  service_version = "${var.service_version}"
+  vault_chef_credentials_path = "secret/credentials/chef"
+	source_image = "ubuntu"
+}
