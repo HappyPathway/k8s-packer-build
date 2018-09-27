0593ad0bb6bf652dcff33d92e0e213bac5c0eafa
David Arnold <dja+djaboxx@sonic.net>
2018-09-27 12:06:21 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "null_resource" "docker_installer" {
 
 module "chef_docker" {
   source  = "app.terraform.io/Darnold-WalMart_Demo/chef-docker/packer"
-  version = "1.2.7"
+  version = "2.0.0"
 
   chef_env = "_default"
   docker_cmd = "${var.docker_cmd}"
